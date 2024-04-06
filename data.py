e1d567028bd649e052f1b649dbdbf338c7e4ab45
hejizhang <hjz921195000@mail.ustc.edu.cn>
2024-04-06 22:15:14 +0800

    改成了各种import的形式.新的main代码是new.ipynb

---
+++ data.py
@@ -0,0 +1,93 @@
+import pandas as pd
+
+# 紫金矿业 江西铜业 铜陵有色 西部矿业 云南铜业
+# 601899 600362 000630 601168 000878
+# CU9999
+pair = {}
+pair['CU9999.XSGE'] = ['601899', '600362', '000630', '601168', '000878']
+
+# 潞安环能 山西焦煤 永泰能源 淮北矿业 华阳股份
+# 601699 000983 600157 600985 600348
+# JM9999
+pair['JM9999.XDCE'] = ['601699', '000983', '600157', '600985', '600348']
+
+# 宝钢股份 包钢股份 华菱钢铁 首钢股份 鞍钢股份
+# 600019 600010 000932 000959 000898
+# HC9999
+pair['HC9999.XSGE'] = ['600019', '600010', '000932', '000959', '000898']
+
+# 中国石油 中国石化 荣盛石化 恒力石化 东方盛虹
+# 601857 600028 002493 600346 000301
+# SC9999
+pair['SC9999.XINE'] = ['601857', '600028', '002493', '600346', '000301']
+
+# 三友化工 吉林化纤 新乡化纤 恒天海龙 南京化纤
+# 600409 000420 000949 000677 600889
+# TA9999
+pair['TA9999.XZCE'] = ['600409', '000420', '000949', '000677', '600889']
+
+# 宝丰能源 鲁西化工 华谊集团 诚志股份 江苏索普
+# 600989 000830 600623 000990 600746
+# L9999
+pair['L9999.XDCE'] = ['600989', '000830', '600623', '000990', '600746']
+
+# 赛轮轮胎 玲珑轮胎 三角轮胎 通用股份 贵州轮胎
+# 601058 601966 601163 601500 000589
+# RU9999
+pair['RU9999.XSGE'] = ['601058', '601966', '601163', '601500', '000589']
+
+# 中粮糖业 冠农股份 南宁糖业 华资实业
+# 600737 600251 000911 600191
+# SR9999
+pair['SR9999.XZCE'] = ['600737', '600251', '000911', '600191']
+
+# 大北农 天康生物 唐人神 禾丰股份 傲农生物
+# 002385 002100 002567 603609 603363
+# M9999
+pair['M9999.XDCE'] = ['002385', '002100', '002567', '603609', '603363']
+
+# 百隆东方 鲁泰 A 华孚时尚 孚日股份 华茂股份
+# 601339 000726 002042 002083 000850
+# CF9999
+pair['CF9999.XZCE'] = ['601339', '000726', '002042', '002083', '000850']
+
+fut_list = ['CU9999.XSGE', 'JM9999.XDCE', 'HC9999.XSGE', 'SC9999.XINE', 'TA9999.XZCE', 'L9999.XDCE', 'RU9999.XSGE', 'SR9999.XZCE', 'M9999.XDCE', 'CF9999.XZCE']
+
+
+
+metal = pd.read_csv("stocks/metal.csv")
+metal['Stkcd'] = metal['Stkcd'].astype(str).str.zfill(6)
+
+coal = pd.read_csv("stocks/coal.csv")
+coal['Stkcd'] = coal['Stkcd'].astype(str).str.zfill(6)
+
+nonferrous = pd.read_csv("stocks/nonferrous.csv")
+nonferrous['Stkcd'] = nonferrous['Stkcd'].astype(str).str.zfill(6)
+
+all_stock = pd.concat([metal, coal, nonferrous], axis=0)
+all_stock.rename(columns={'Trddt': 'date', 'Opnprc': 'open','Loprc':'low', 'Hiprc':'high', 'Clsprc':'close'}, inplace=True)
+all_stock['date'] = pd.to_datetime(all_stock['date'])
+
+
+def fut_read(x):
+    fut = pd.read_csv(f"futures/{x}.csv")
+    fut['date'] = pd.to_datetime(fut['date'])
+    fut['ChangeRatio'] = fut['close'].diff() / fut['close'].shift(1) 
+    fut['ChangeRatio'].iloc[0] = 0
+    fut = fut[fut.date>='2019-05-01']
+    return fut
+
+def stock_read(x):
+    dfs = []
+    stock_df = []
+    stock_codes = pair[x]
+    # print(len(stock_codes))
+    for code in stock_codes:
+        df = all_stock[all_stock["Stkcd"] == code]
+        df = df[df.date>='2019-05-01']
+        stock_df.append(df)
+    return stock_df
+# 注意！有些期货对应的股票没数据。
+
+if __name__ == '__main__':
+    pass
---
+++ util.py
@@ -0,0 +1,88 @@
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.api import VAR
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
+
+def adf_test(timeseries):
+    # print('Results of Augmented Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic', 'p-value', '#Lags Used', 'Number of Observations Used'])
+    for key, value in dftest[4].items():
+        dfoutput['Critical Value (%s)' % key] = value
+    print(dfoutput)
+    return dfoutput
+
+def johansen_cointegration_test(df, det_order = -1, k_ar_diff = 1):
+    """Perform Johansen cointegration test on dataframe df
+    det_order : int
+        * -1 - no deterministic terms
+        *  0 - constant term
+        *  1 - linear trend
+    k_ar_diff : int, number of lagged differences in the model.
+    """
+    print('Results of Johansen Cointegration Test:')
+    result = coint_johansen(df, det_order, k_ar_diff)
+    print(f"Test statistic: {result.lr1}")
+    print(f"Critical values: {result.cvt}")
+    print(f"Eigenstatistics: {result.lr2}")
+    print(f"Eigenvalues: {result.eig}")
+
+# 假设你有一个名为df的Pandas DataFrame时间序列数据集，每列是一个时间序列
+# johansen_cointegration_test(df)
+
+
+# 根据fut 跟 stock 的df ，以及feature。生成需要的数据
+def data_generator(fut_df, stock_df, feature='close'):
+    # fut = 'CU9999'
+    # stock = '600362'
+    # print(fut, stock)
+    # au = pd.read_csv(f"futures/{fut}.csv")
+    # print(len(au))
+    # au['date'] = pd.to_datetime(au['date'])
+    # au['ChangeRatio'] = au['close'].diff() / au['close'].shift(1) 
+    # au['ChangeRatio'].iloc[0] = 0
+    
+    
+    # au = au[au.date>='2019-05-01']
+    #stock_df = stock_read(fut)
+    # print(len(stock_df[1]))
+    # sig_col = []
+    # for idx, df in enumerate(stock_df):
+    # stock_df['date'] = pd.to_datetime(stock_df['date'])
+    # df = df[df.date>='2019-05-01']
+    # df.rename(columns={"Trddt": "date"}, inplace=True)
+    merged_df = pd.concat([fut_df['date'], stock_df['date']])
+    unique_times = merged_df.drop_duplicates()
+    unique_times[~unique_times.isin(fut_df['date']) | ~unique_times.isin(stock_df['date'])]
+    # p_value_list = []
+
+    unique_times_df1 = set(fut_df['date'])
+    unique_times_df2 = set(stock_df['date'])
+    common_times = list(unique_times_df1.intersection(unique_times_df2))
+    df1_common = fut_df[fut_df['date'].isin(common_times)]
+    df2_common = stock_df[stock_df['date'].isin(common_times)]
+    merged_df = pd.merge(df1_common, df2_common, on='date', suffixes=('_df1', '_df2'))
+    data = merged_df[[f'{feature}_df1', f'{feature}_df2']]
+    return data
+
+def significant(data):
+    p_value_list = []
+    num_days = len(data)
+    window_size = 30
+    for i in range(num_days - window_size + 1):
+        window_start = i
+        window_end = i + window_size
+        window_data = data.iloc[window_start:window_end]
+        model = VAR(window_data)
+        results = model.fit()
+        # print(results.summary())
+        p_values = results.pvalues    
+        p_value_list.append(np.array(p_values.iloc[:,0]))
+    significant_columns = [i for i, arr in enumerate(p_value_list) if all(arr < 0.05)]
+    return significant_columns
+
+
+if __name__ == '__main__':
+    pass
